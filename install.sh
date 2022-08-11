1df01de6ee54f06e9d3eca0e9c24f0f41f69740e
Joshua Lowe <joshualowe1002@gmail.com>
2022-08-11 14:38:43 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,16 +1,20 @@
+sudo apt update
 sudo apt install hostapd
 sudo systemctl unmask hostapd
 sudo systemctl enable hostapd
 sudo apt install dnsmasq
 sudo DEBIAN_FRONTEND=noninteractive apt install -y netfilter-persistent iptables-persistent
-sudo cp -f /etc/dhcpcd.conf files/dhcpcd.conf
-sudo cp -f /etc/sysctl.d/routed-ap.conf files/routed-ap.conf
+sudo cp -f files/dhcpcd.conf /etc/dhcpcd.conf
+sudo touch /etc/sysctl.d/routed-ap.conf
+sudo cp -f files/routed-ap.conf /etc/sysctl.d/routed-ap.conf
 sudo iptables -t nat -A POSTROUTING -o eth0 -j MASQUERADE
 sudo netfilter-persistent save
 sudo mv /etc/dnsmasq.conf /etc/dnsmasq.conf.orig
-sudo cp -f /etc/dnsmasq.conf files/dnsmasq.conf
+sudo touch /etc/dnsmasq.conf
+sudo cp -f files/dnsmasq.conf /etc/dnsmasq.conf
 sudo rfkill unblock wlan
 SERIALSSID=$(cat /sys/firmware/devicetree/base/serial-number | tail -c -8)
 echo "ssid=FC-$SERIALSSID" >> files/hostapd.conf
-sudo cp -f /etc/hostapd/hostapd.conf files/hostapd.conf
+sudo touch /etc/hostapd/hostapd.conf
+sudo cp -f files/hostapd.conf /etc/hostapd/hostapd.conf
 sudo systemctl reboot
